a8a8c7c1f489e5c015754454fd07d7f9d001e67c
Patricio-L <patricio822@gmail.com>
2022-07-26 19:35:46 -0400

    se completaron detalles diseño

--- app/src/main/java/com/pat/cartapresentacion/MainActivity.kt
+++ app/src/main/java/com/pat/cartapresentacion/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,8 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -86,7 +90,7 @@ fun CrearCarta(){
     }
 }
 
-@Preview
+
 @Composable
 fun Contenido(){
     Box(modifier = Modifier
@@ -112,7 +116,24 @@ fun Portafolio(datos: List<String>) {
 LazyColumn{
     items(datos){
          item ->
-        Text(item)
+         Card(modifier = Modifier
+             .padding(15.dp)
+             .fillMaxWidth(),
+              shape = RectangleShape,
+              elevation = 4.dp) {
+             Row(modifier = Modifier
+                 .padding(8.dp)
+                 .background(MaterialTheme.colors.surface)
+                 .padding(7.dp)) {
+                 CrearImagenPerfil(modifier = Modifier.size(100.dp))
+                 Column(modifier = Modifier.padding(7.dp)
+                     .align(alignment = Alignment.CenterVertically)) {
+                     Text(text = item, fontWeight = FontWeight.Bold)
+                     Text(text = "En Jetpack Compose",
+                     style = MaterialTheme.typography.body2)
+                 }
+             }
+         }
     }
 }
 }
@@ -141,7 +162,7 @@ private fun MostrarDatosPersonales() {
 @Composable
 private fun CrearImagenPerfil(modifier: Modifier = Modifier) { //comentario
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -152,7 +173,7 @@ private fun CrearImagenPerfil(modifier: Modifier = Modifier) { //comentario
         Image(
             painter = painterResource(id = R.drawable.imagen_perfil1),
             contentDescription = "imagen de perfil",
-            modifier = Modifier.size(150.dp),
+            modifier = modifier.size(150.dp),
             contentScale = ContentScale.Crop
         )
     }
